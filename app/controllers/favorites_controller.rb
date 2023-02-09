af23547d46fc2adc16bd2928fe897e225c5ddd35
Arturo Duarte <72163096+arturo-bucksapp@users.noreply.github.com>
2023-02-09 11:04:20 -0300

    43 - Refactorizando con turbo - streams

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,31 @@
 class FavoritesController < ApplicationController
   def index
   end
-  
+
   def create
     product.favorite!
-    redirect_to(product_path(product))
+    respond_to do |format|
+      format.html { redirect_to(product_path(product)) }
+      format.turbo_stream do
+        # el primer favorite se refiere al id del html a reemplazar
+        render(
+          turbo_stream: turbo_stream.replace('favorite', partial: 'products/favorite', locals: { product: product })
+        )
+      end
+    end
   end
 
   def destroy
     product.unfavorite!
-    redirect_to(product_path(product), status: :see_other)
+    respond_to do |format|
+      format.html { redirect_to(product_path(product), status: :see_other) }
+      format.turbo_stream do
+        # el primer favorite se refiere al id del html a reemplazar
+        render(
+          turbo_stream: turbo_stream.replace('favorite', partial: 'products/favorite', locals: { product: product })
+        )
+      end
+    end
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by!(username: params[:username])
-    @categories = ::Category.order(name: :asc).load_async
-    @pagy, @products = pagy_countless(::FindProducts.new.call({ user_id: @user.id }).load_async, items: 5)
   end
 end
