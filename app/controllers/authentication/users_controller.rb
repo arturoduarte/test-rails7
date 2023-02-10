e78d80a77aa1d3f5a3e10dd4884936b6eea9d5f8
Arturo Duarte <72163096+arturo-bucksapp@users.noreply.github.com>
2023-02-10 15:52:53 -0300

    51 - Enviando emauls con ActionMailer

--- app/controllers/authentication/users_controller.rb
+++ app/controllers/authentication/users_controller.rb
@@ -9,6 +9,7 @@ module Authentication
     def create
       @user = ::User.new(user_params)
       if @user.save
+        UserMailer.with(user: @user).welcome.deliver_later
         session[:user_id] = @user.id
         redirect_to(products_path, notice: t('.created'))
       else
